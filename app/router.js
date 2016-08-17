6c566b2343421519eeb8f1e6dbd18b827ab04abf
George Olson and Shradha Pulla <george@>
2016-08-17 10:10:53 -0700

    Add bootstrap, front end styles, navbar

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('admin');
+  this.route('post');
 });
 
 export default Router;
